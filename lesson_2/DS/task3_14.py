646746a848aa0031d223b83a8b96eee875d4381a
Gary <45505432+Argentovskiy@users.noreply.github.com>
2023-10-20 11:38:36 +0300

    Update task3_14.py

--- lesson_2/DS/task3_14.py
+++ lesson_2/DS/task3_14.py
@@ -2,3 +2,12 @@
 # вида 2k
 # ), не превосходящие числа N.
 # 10 -> 1 2 4 8
+import os
+os.system("clear")
+n=256
+
+sum=1
+for i in range(1,n):
+    if sum<=n:
+        print(sum)
+        sum=sum*2
